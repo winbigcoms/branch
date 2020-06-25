aac5965b51670a4693dc518b574a6552b8e7fde9
jxr208 <jxr208@gmail.com>
2020-06-25 20:47:07 +0900

    project

--- projectclient/src/personal/yong.js
+++ projectclient/src/personal/yong.js
@@ -1,7 +1,23 @@
-const togglesidebar = () => {
+const openSidebar = document.querySelector(".btn-menu.img-ico");
+const banner = document.querySelector('.banner')
+const link1 = document.querySelector('.link-page.link-page1');
+const link2 = document.querySelector('.link-page.link-page2');
+
+
+openSidebar.onclick = togglesidebar = () => {
     document.getElementById('sidemenu').classList.toggle('open');
 };
 
 const btnClose = () => {
-    document.getElementsByClassName('banner')[0].setAttribute('style', 'margin-top: -420px');
+    banner.style.marginTop = "-420px"
 };
+
+link1.onclick = () => {
+    document.getElementsByClassName('item1')[0].setAttribute('style', 'z-index: 2');
+    document.getElementsByClassName('item2')[0].setAttribute('style', 'z-index: 1');
+}
+
+link2.onclick = () => {
+    document.getElementsByClassName('item1')[0].setAttribute('style', 'z-index: 1');
+    document.getElementsByClassName('item2')[0].setAttribute('style', 'z-index: 2');
+}
